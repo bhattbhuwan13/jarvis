584125fbf9d937351dcdae5b314bf1a6687ac4e3
Bhuwan <bhattbhuwan13@outlook.com>
2024-04-10 23:27:01 +0100

    minor refactor

--- jarvis/notebooks/langchain01.py
+++ jarvis/notebooks/langchain01.py
@@ -1,8 +1,11 @@
+from langchain.chains import create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_community.document_loaders import WebBaseLoader
+from langchain_community.vectorstores import FAISS
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
-
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 llm = ChatOpenAI()
 
@@ -11,15 +14,12 @@ docs = loader.load()
 
 embeddings = OpenAIEmbeddings()
 
-from langchain_community.vectorstores import FAISS
-from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 text_splitter = RecursiveCharacterTextSplitter()
 
 documents = text_splitter.split_documents(docs)
 vector = FAISS.from_documents(documents, embeddings)
 
-from langchain.chains.combine_documents import create_stuff_documents_chain
 
 prompt = ChatPromptTemplate.from_template(
     """Answer the following question based only on the provided context:
@@ -33,7 +33,6 @@ Question: {input}"""
 
 document_chain = create_stuff_documents_chain(llm, prompt)
 
-from langchain.chains import create_retrieval_chain
 
 retriever = vector.as_retriever()
 retrieval_chain = create_retrieval_chain(retriever, document_chain)
